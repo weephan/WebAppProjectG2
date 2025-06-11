4d20926e90ac4eb53d9788738b74f53fce828a8a
faraathrh <faraathrh@gmail.com>
2025-06-12 00:26:57 +0800

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ReportController;
+use App\Http\Controllers\MenuController;
 
-// Route::get('/reports/monthly', [ReportController::class, 'showMonthlyReport']);
-//new /latest ver
-// Route::get('/', [ReportController::class, 'showMonthlyReport']);
-Route::get('/', [ReportController::class, 'showReports'])->name('reports');
+// Main Page Route
+Route::get('/', function () {
+    return view('mainpage');
+})->name('home');
 
+Route::resource('menu', MenuController::class);
+Route::get('/add-menu', [MenuController::class, 'create'])->name('menu.add');
 
-//Route::get('/reports/monthly', [ReportController::class, 'showMonthlyReport']);
-// Route::get('/reports/monthly', [ReportController::class, 'showMonthlyReport'])->name('reports.monthly');
-
-// Route::get('/', function () {
-//     return view('report');
-// });
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
