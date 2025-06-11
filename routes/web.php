7c6e6d6cce7a143e5e2e629c619234a2bb333e3e
Yana <139585616+eirdyana@users.noreply.github.com>
2025-06-12 01:39:13 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,4 @@ Route::get('/', function () {
 
 // // RESTful routes for inventory
 Route::resource('inventory', InventoryController::class);
-// Route::get('/inventory', [InventoryController::class, 'index'])->name('inventory.index');
-// For Resourceful Routes (Recommended)
-// Route::resource('inventory', 'App\Http\Controllers\InventoryController');
 
-// // OR Manually Define Each Route
-// Route::get('/inventory', [InventoryController::class, 'index'])->name('inventory.index');
-// Route::get('/inventory/create', [InventoryController::class, 'create'])->name('inventory.create');
-// Route::post('/inventory', [InventoryController::class, 'store'])->name('inventory.store');
-// Route::get('/inventory/{id}/edit', [InventoryController::class, 'edit'])->name('inventory.edit');
-// Route::put('/inventory/{id}', [InventoryController::class, 'update'])->name('inventory.update');
-// Route::delete('/inventory/{id}', [InventoryController::class, 'destroy'])->name('inventory.destroy');
