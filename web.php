fc253355151110175f64a6404be9f9692af55403
Yana <139585616+eirdyana@users.noreply.github.com>
2025-06-11 18:24:19 +0800

    INVENTORY (route)

--- web.php
+++ web.php
@@ -1,41 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\POSController;
+use App\Http\Controllers\InventoryController;
 
+// Main page
 Route::get('/', function () {
-    return view ('mainpage');;
+    return view('mainpage');
 });
 
-// POS main page
-Route::get('/POS', [POSController::class, 'index'])->name('POS');
-
-// Add item to cart
-Route::post('/POS/add', [POSController::class, 'addToCart'])->name('add.to.cart');
-
-// View checkout page
-Route::get('/checkout', [POSController::class, 'checkout'])->name('checkout');
-
-// Process checkout (store order, etc.)
-Route::post('/checkout/process', [POSController::class, 'processCheckout'])->name('checkout.process');
-
-// Show receipt
-Route::get('/receipt', function () {
-    return view('receipt');
-})->name('receipt');
-
-// Remove specific item from cart
-Route::get('/removeFromCart/{index}', function ($index) {
-    $cart = session('cart', []);
-    unset($cart[$index]); // remove by array index
-    session(['cart' => array_values($cart)]); // reindex
-    return redirect('/POS'); // or your POS route
-})->name('cart.remove');
-
-
-Route::get('/cart/clear', [POSController::class, 'clearCart'])->name('cart.clear');
-
-Route::get('/empty-cart', function () {
-    session()->forget('cart');
-    return redirect()->route('POS')->with('success', 'Cart emptied.');
-})->name('cart.empty');
+// RESTful routes for inventory
+Route::resource('inventory', InventoryController::class);
