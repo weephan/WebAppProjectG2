489afe16404ca5a0ae6a9b0c4ab5ee0603d49c08
faraathrh <faraathrh@gmail.com>
2025-06-12 01:54:14 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InventoryController;
+use App\Http\Controllers\MenuController;
 
 // Main page
 Route::get('/', function () {
@@ -10,4 +11,7 @@ Route::get('/', function () {
 
 // // RESTful routes for inventory
 Route::resource('inventory', InventoryController::class);
+
+Route::resource('menu', MenuController::class);
+Route::get('/add-menu', [MenuController::class, 'create'])->name('menu.add');
 
