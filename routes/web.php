5b982a7d49dc00c50d749abfd20ad171c8c35dbb
weephan <ummuhanie89@gmail.com>
2025-06-12 00:40:07 +0800

    change location and web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MenuController;
+use App\Http\Controllers\LoyaltyController;
+use App\Http\Controllers\ReportController;
+
+Route::get('/', [ReportController::class, 'showReports'])->name('reports');
 
 // Main Page Route
 Route::get('/', function () {
@@ -11,3 +15,11 @@ Route::get('/', function () {
 Route::resource('menu', MenuController::class);
 Route::get('/add-menu', [MenuController::class, 'create'])->name('menu.add');
 
+Route::prefix('loyalty')->group(function () {
+    Route::get('/', [LoyaltyController::class, 'index'])->name('index');
+    Route::get('/create', [LoyaltyController::class, 'create'])->name('create');
+    Route::post('/', [LoyaltyController::class, 'store'])->name('store');
+    Route::get('/{id}', [LoyaltyController::class, 'show'])->name('show');
+    Route::post('/{customerId}/transaction', [LoyaltyController::class, 'addTransaction'])
+        ->name('loyalty.addTransaction');
+});
